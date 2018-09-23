9cb83f3192cadfdca0d8a2e4f4447441e630f707
zecollokaris <collo.kariss@gmail.com>
2018-09-23 15:12:33 +0300

    added client id and client secret

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -5,4 +5,10 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class GithubService{
     private username = 'zecollokaris'
+    private client_id = 'dbf81d910a3a2a14e3e1';
+    private client_secret = 'ae701bfc8e1d481e8bfd349a2aebf4cbe1c9f87d';
+
+    constructor(private _http:Http){
+        console.log('Github Service Init...');
+    }
 }
