4b88a79b9dc9a546969ead05051c16f2460667a1
zecollokaris <collo.kariss@gmail.com>
2018-09-23 20:16:38 +0300

    added service for getting repos

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -16,4 +16,10 @@ export class GithubService{
         return this._http.get('https://api.github.com/users/'+this.username)
             .map(res => res.json());
     }
+
+    getRepos() {
+        return this._http.get('https://api.github.com/users/' + this.username + '/repos')
+            .map(res => res.json());
+    }
+
 }
