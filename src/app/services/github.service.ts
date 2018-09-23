77044525744cf08fe4e0ecb20807849fe0c6b53e
zecollokaris <collo.kariss@gmail.com>
2018-09-23 15:06:58 +0300

    added injectable to service.ts

---
+++ src/app/services/github.service.ts
@@ -0,0 +1,8 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+    private username = 'zecollokaris'
+}
